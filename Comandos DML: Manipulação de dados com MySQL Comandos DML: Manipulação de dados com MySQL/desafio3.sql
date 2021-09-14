2e9650289404c0590c575f5a99c6312b8090fa2f
Bruno Duarte <brunoarduarte@gmail.com>
2021-09-14 09:53:22 -0300

    Alter table

---
+++ Comandos DML: Manipulação de dados com MySQL Comandos DML: Manipulação de dados com MySQL/desafio3.sql
@@ -0,0 +1,7 @@
+ALTER TABLE itens_notas_fiscais RENAME ITENS_NOTAS;
+
+ALTER TABLE tabela_de_vendedores RENAME VENDEDORES;
+
+ALTER TABLE tabela_de_clientes RENAME CLIENTES;
+
+ALTER TABLE tabela_de_produtos RENAME PRODUTOS;
